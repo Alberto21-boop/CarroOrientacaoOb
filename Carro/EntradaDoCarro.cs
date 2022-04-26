d1b34dd68a7f5741f4566d64de44b2a01d429925
alberto <albertobarbosa0003@gmail.com>
2022-04-26 09:39:58 -0300

    Adicionar arquivos de projeto.

---
+++ Carro/EntradaDoCarro.cs
@@ -0,0 +1,13 @@
+﻿using Carro;
+
+CarroEstacionamento carroestacionamento = new CarroEstacionamento();
+
+carroestacionamento.Nome = Console.ReadLine();
+carroestacionamento.Cor = Console.ReadLine();
+carroestacionamento.QuantidadeDePortas = int.Parse(Console.ReadLine());
+carroestacionamento.Placa = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Carro : {carroestacionamento.NomeDoCarro()}");
+Console.WriteLine($"Cor do Carro : {carroestacionamento.CorDoCarro()}");
+Console.WriteLine($"Numero de Portas : {carroestacionamento.PortasDoCarro()}");
+Console.WriteLine($"Placa do Veiculo : {carroestacionamento.NumerosDasPlaca()}");
---
+++ Carro/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Carro
+{
+    internal class CarroEstacionamento
+    {
+        public string Nome { get; set; } = default!;
+        public string Cor { get; set; } = default!;
+        public int QuantidadeDePortas { get; set; }
+        public int Placa { get; set; }
+
+        public string NomeDoCarro()
+        {
+            return Nome;
+        }
+        public string CorDoCarro()
+        {
+            return Cor;
+        }
+        public int PortasDoCarro()
+        {
+            return QuantidadeDePortas;
+        }
+        public int NumerosDasPlaca()
+        {
+            return Placa;
+        }
+    }
+}
